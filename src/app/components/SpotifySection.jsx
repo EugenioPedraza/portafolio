214c30ef33e59eb193d889b7555efa92e3284bb7
EugenioPedraza <a00835064@tec.mx>
2024-08-05 08:06:46 -0600

    Probando API correo

--- src/app/components/SpotifySection.jsx
+++ src/app/components/SpotifySection.jsx
@@ -8,7 +8,6 @@ const SpotifySection = () => {
         src="https://open.spotify.com/embed/playlist/49Cg8XlXuKNfWpYgwAt9hm?utm_source=generator"
         width="100%"
         height="352"
-        frameBorder="0"
         allowFullScreen
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy">
